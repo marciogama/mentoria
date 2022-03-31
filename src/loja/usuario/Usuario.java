db24b543396f199f4294ccb872b4921ad36be350
Rodrigo Nascimento <rodrigo_allexandre@yahoo.com.br>
2022-03-31 17:40:36 -0300

    teste git usuario

--- src/loja/usuario/Usuario.java
+++ src/loja/usuario/Usuario.java
@@ -1,4 +1,25 @@
 package loja.usuario;
 
 public class Usuario {
+
+    private String nome;
+
+    public Usuario(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
 }
